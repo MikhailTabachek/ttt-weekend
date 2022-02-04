3bb540c9796929883bd0e9f3a9bc1da1f7d630da
Mikhail <mishanyaveselyi@gmail.com>
2022-02-04 14:45:30 -0500

    Set Cashed Elements

--- js/app.js
+++ js/app.js
@@ -3,18 +3,27 @@
 
 
 /*---------------------------- Variables (state) ----------------------------*/
-let squares
+let squareArr
 let turn
 let winner
 
 
 
 /*------------------------ Cached Element References ------------------------*/
+const message = document.getElementById("message")
+// const square0 = document.getElementById("sq0")
+// const square1 = document.getElementById("sq1")
+// const square2 = document.getElementById("sq2")
+// const square3 = document.getElementById("sq3")
+// const square5 = document.getElementById("sq5")
+// const square6 = document.getElementById("sq6")
+// const square7 = document.getElementById("sq7")
+// const square8 = document.getElementById("sq8")
 
 
 
 /*----------------------------- Event Listeners -----------------------------*/
-let xO = document.onclick
+
 
 
 /*-------------------------------- Functions --------------------------------*/
