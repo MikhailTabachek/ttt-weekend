0f0f739a1454cf20a91e1613ce7ba1891e97c4fc
Mikhail <mishanyaveselyi@gmail.com>
2022-02-04 17:53:18 -0500

    Set Handle Click

--- js/app.js
+++ js/app.js
@@ -1,11 +1,9 @@
 /*-------------------------------- Constants --------------------------------*/
-
+const winArr = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
 
 
 /*---------------------------- Variables (state) ----------------------------*/
-let squareArr
-let turn
-let winner
+let squareArr, turn, winner
 
 
 
@@ -24,7 +22,49 @@ const message = document.getElementById("message")
 
 /*----------------------------- Event Listeners -----------------------------*/
 
-
+document.querySelectorAll(".square").forEach(element => element.addEventListener("click", handleClick))
 
 /*-------------------------------- Functions --------------------------------*/
 
+init();
+
+function init(){
+  squareArr = [1, 1, 1, null, null, null, null, null, null]
+  turn = 1
+  winner = null
+  // render();
+// console.log()
+}
+
+console.log()
+
+function handleClick(evt){
+// if turn > 0 &&  => -1,  else if turn <0 => 1
+let index = evt.target.id[2]
+if (squareArr[index] || winner){
+  return
+}
+
+squareArr[index] = turn
+
+turn *= -1
+
+console.log(squareArr)
+}
+
+function render(){
+
+}
+
+
+//  arr1 = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
+// arr1.forEach(function(win){
+// win.forEach(winWin){
+// compare if element with index 0 = 0 && 1 = 1 && = 2 
+// }
+// })
+
+// arr2 = [1, 1, 1, -1, null, null, null, null, null]
+// arr2 = [0, 1, 2,    3,    4,    5,    6,    7,    8]
+
+// if index 0 in array 2 = 1 
